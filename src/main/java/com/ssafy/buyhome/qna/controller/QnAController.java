2d7692e2240c37393e74765457939c459a25e634
qww1552 <qww1552@naver.com>
2022-11-21 16:15:33 +0900

    qna 생성

--- src/main/java/com/ssafy/buyhome/qna/controller/QnAController.java
+++ src/main/java/com/ssafy/buyhome/qna/controller/QnAController.java
@@ -1,12 +1,10 @@
 package com.ssafy.buyhome.qna.controller;
 
+import com.ssafy.buyhome.qna.model.dto.Question;
 import com.ssafy.buyhome.qna.model.service.QnaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/board/qna")
@@ -23,5 +21,11 @@ public class QnAController {
     @GetMapping("/{qnaid}")
     public ResponseEntity<?> findById(@PathVariable("qnaid") Integer qnaId) {
         return ResponseEntity.ok(qnaService.findById(qnaId));
+    }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Question question) {
+        qnaService.create(question);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/com/ssafy/buyhome/qna/model/service/QnaService.java
+++ src/main/java/com/ssafy/buyhome/qna/model/service/QnaService.java
@@ -22,4 +22,8 @@ public class QnaService {
     public Question findById(int id) {
         return questionDao.select(id);
     }
+
+    public void create(Question question) {
+        questionDao.insert(question);
+    }
 }
