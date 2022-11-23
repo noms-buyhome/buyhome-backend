942517047c3b88af12350cf862ad85ba486a4ede
qww1552 <qww1552@naver.com>
2022-11-23 14:44:00 +0900

    회원 삭제

--- src/main/java/com/ssafy/buyhome/user/model/service/UserService.java
+++ src/main/java/com/ssafy/buyhome/user/model/service/UserService.java
@@ -1,28 +1,41 @@
 package com.ssafy.buyhome.user.model.service;
 
+import com.ssafy.buyhome.qna.model.dao.QuestionDao;
+import com.ssafy.buyhome.qna.model.dto.Question;
 import com.ssafy.buyhome.user.model.dao.UserDao;
 import com.ssafy.buyhome.user.model.dto.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserDao userDao;
+    private final QuestionDao questionDao;
 
+    @Transactional
     public User findById(int userId) {
         return userDao.select(userId);
     }
 
+    @Transactional
     public User findByUsername(String username) {
         return userDao.selectByUsername(username);
     }
+
+    @Transactional
     public void update(Integer userId, User incomingData) {
         userDao.update(userId, incomingData);
     }
 
+    @Transactional
     public void delete(Integer userId) {
+        for (Question question : questionDao.selectAllByUserID(userId)) {
+            questionDao.deleteAnswerByQuestionId(question.getId());
+        }
+        questionDao.deleteQuestionByUserId(userId);
         userDao.delete(userId);
     }
 }
